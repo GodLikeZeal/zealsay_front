be8e85f364042f066c32a6718d101d97cc66e8a4
zhanglei <zhanglei@i-counting.cn>
2019-05-28 22:32:44 +0800

    [feat] 增加依赖

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-import colors from 'vuetify/es5/util/colors'
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import zhHans from 'vuetify/es5/locale/zh-Hans'
+import '@mdi/font/css/materialdesignicons.css'
+import { Ripple } from 'vuetify/lib/directives'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: colors.blue.darken2,
-    accent: colors.grey.darken3,
-    secondary: colors.amber.darken3,
-    info: colors.teal.lighten1,
-    warning: colors.amber.base,
-    error: colors.deepOrange.accent4,
-    success: colors.green.accent3
+    primary: '#4caf50',
+    secondary: '#4caf50',
+    tertiary: '#495057',
+    accent: '#82B1FF',
+    error: '#f55a4e',
+    info: '#00d3ee',
+    success: '#5cb860',
+    warning: '#ffa21a'
+  },
+  iconfont: 'mdi',
+  directives: {
+    Ripple
+  },
+  lang: {
+    locales: { zhHans },
+    current: 'zh-Hans'
   }
 })
