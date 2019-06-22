269d087d87f46b490e2f519f6ae2c07ef84c88b0
zhanglei <zhanglei@i-counting.cn>
2019-06-23 00:31:35 +0800

    [feat] 首页布局修改

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -8,6 +8,7 @@ import Avatar from 'vue-avatar'
 import Card from '~/components/material/Card'
 import ChartCard from '~/components/material/ChartCard'
 import StatsCard from '~/components/material/StatsCard'
+import Footer from '~/components/core/Footer'
 import VAvatarUploader from 'vuetify-avatar-uploader'
 
 Vue.use(Vuetify, {
@@ -34,5 +35,6 @@ Vue.use(Vuetify, {
 Vue.component('material-card', Card)
 Vue.component('material-chart-card', ChartCard)
 Vue.component('material-stats-card', StatsCard)
+Vue.component('core-footer', Footer)
 Vue.component('avatar', Avatar)
 Vue.component('v-avatar-uploader', VAvatarUploader)
