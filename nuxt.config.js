5d86814eb7a0ea432edc8791890f5dc9026e7cd7
zhanglei <zhanglei@i-counting.cn>
2019-09-16 21:08:33 +0800

    [feat] 更新部署参数传递

--- nuxt.config.js
+++ nuxt.config.js
@@ -151,7 +151,7 @@ export default {
 
   proxy: {
     '/app/': {
-      target: 'https://api.zealsay.com', // 目标接口域名
+      target: process.env.VUE_APP_API_URL, // 目标接口域名
       // target: 'http://localhost:8090', // 目标接口域名
       changeOrigin: true, // 是否跨域
       pathRewrite: { '^/app/': '' } // 把/api 替换成 /
