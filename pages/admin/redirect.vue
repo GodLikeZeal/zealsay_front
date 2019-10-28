fbd1b5da3ba9abbd557a50cf80ac6d1b374311d0
zhanglei <zhanglei@i-counting.cn>
2019-10-28 19:14:57 +0800

    [feat] 更新注册页面

---
+++ pages/admin/redirect.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app id="conform">
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center row>
+        <div class="text-md-center">
+          <div>
+            <v-progress-circular
+              :size="70"
+              color="primary"
+              indeterminate
+            ></v-progress-circular>
+          </div>
+          <h2>登录跳转中...</h2>
+        </div>
+      </v-layout>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'Redirect',
+  auth: false,
+  // eslint-disable-next-line require-await
+  created() {
+    if (!this.$isServer) {
+      const token = this.$route.query.token
+      // eslint-disable-next-line no-console
+      console.log('token是:' + token)
+      const redirect = this.$route.query.redirect
+      this.$auth.setUserToken(token).then(() => {
+        // eslint-disable-next-line no-console
+        console.log('跳转啊啊啊啊')
+        this.$router.push({
+          path: redirect || '/'
+        })
+      })
+    }
+  }
+}
+</script>
+<style scoped lang="css">
+h1 {
+  font-size: 150px;
+  line-height: 150px;
+  font-weight: 700;
+  color: #252932;
+  text-shadow: rgba(61, 61, 61, 0.3) 1px 1px, rgba(61, 61, 61, 0.2) 2px 2px, rgba(61, 61, 61, 0.3) 3px 3px;
+}
+</style>
